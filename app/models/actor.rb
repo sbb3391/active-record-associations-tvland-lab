772bd43c38985b77e89fbe10bc01e101c7f18d5c
Morgan VanYperen <morgvanny@gmail.com>
2021-01-21 12:12:17 -0800

    commit tree c81740fd69bc0434340bd60bb9cc1337b0eecd1a

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+  has_many :networks, through: :shows
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    require 'pry'
+    binding.pry
+    self.characters.includes(:show).map do |c|
+      "#{c.name} - #{c.show.name}"
+    end
+  end
+
 end
