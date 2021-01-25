5e6ba2c6ba8d7f51fcb923754925cdeb0e34da0c
sbb3391 <sbb33391@gmail.com>
2021-01-25 11:43:50 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
+  belongs_to :show
+  belongs_to :actor
   
+  def catchphrase=(catchphrase)
+    @catchphrase = catchphrase
+  end
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map {|character| Actor.find(character.actor_id).full_name}
+  end
+
   
 end
